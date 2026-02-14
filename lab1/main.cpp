d510df5e460442d29ff4665a0f2018ce0539a3d5
degem7 <96479423+degem7@users.noreply.github.com>
2026-02-14 20:25:15 +0300

    Выполнена первая лабораторная

---
+++ lab1/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, ".UTF-8");
+
+    unsigned int n;
+    cout << "Введите n: ";
+    cin >> n;
+
+    unsigned int a = 1, b;
+
+    for(unsigned int i = 2; i < n; i++) {
+        if (n % i == 0) {
+            a = n / i;
+            break;
+        }
+    }
+
+    b = n - a;
+
+    cout << a << " " << b << endl;
+
+    cout << "Бергер Денис Максимович, 090304-РПИа-025" << endl;
+
+    cin.get();
+    while (cin.get() != '\n');
+
+    return 0;
+}
