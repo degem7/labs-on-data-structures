4b0a5878dd2dd363d106bd157ebdd8428ad9d502
degem7 <96479423+degem7@users.noreply.github.com>
2026-02-15 10:15:57 +0300

    Выполнена лёгкая оптимизация

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -12,7 +12,7 @@ int main()
 
     unsigned int a = 1, b;
 
-    for(unsigned int i = 2; i < n; i++) {
+    for(unsigned int i = 2; i <= n/2; i++) {
         if (n % i == 0) {
             a = n / i;
             break;
